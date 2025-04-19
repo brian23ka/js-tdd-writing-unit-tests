6009c3ad5cb537f8650a87a784dff39ba30d36ee
Brian Kaloki <brianngenye@gmail.com>
2025-04-20 00:54:18 +0300

    my firdt commit

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
 // Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      // case-insensitive vowel check
+      points += /[aeiou]/i.test(char) ? 1 : 2;
+    }
+    return points;
+  }
+  
